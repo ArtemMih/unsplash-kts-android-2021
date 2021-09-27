eaaa63c669661f6c0edce12ab37cd87c1fa9c63a
Mikhailov Artyom <artjm20012001@mail.ru>
2021-09-27 23:47:53 +0300

    hw-2 done

--- app/src/main/java/com/artem/unsplash/MainActivity.kt
+++ app/src/main/java/com/artem/unsplash/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.artem.unsplash
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
